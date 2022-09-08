60940f88911f63683209054c931102682634a121
Odoyo7 <ody.daniels7@gmail.com>
2022-09-08 15:41:50 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Retrun: 0
+ */
+int main (void)
+{
+	/*
+printing out a string using */
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
