d0781d8d2c4fb062f061bbc452478ad2053d06ff
Odoyo7 <ody.daniels7@gmail.com>
2022-09-09 09:16:48 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
  *
  * Retrun: 0
  */
-int main (void)
+int main(void)
 {
-	/*
-printing out a string using */
+	/*printing out a string using*/
 	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 
 }
