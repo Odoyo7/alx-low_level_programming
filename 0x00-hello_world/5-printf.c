da0c001b16d13c749b2bae39af57e98b65e847c6
Odoyo7 <ody.daniels7@gmail.com>
2022-09-09 10:07:55 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	/* printing to the console using printf*/
-	printf("with proper grammar but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
