fc334302d71703316c28741ca798854b4f71563f
Odoyo7 <ody.daniels7@gmail.com>
2022-09-23 10:05:17 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- *Return: 0 always (success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 
-	return (0);
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
 }
